dc914690b2a50d7d0c05bffe291538f55937ce29
arkuuu <arkuuu@users.noreply.github.com>
2014-10-28 19:24:15 +0100

    fixed extra whitespace in getName()
    there was a extra whitespace at the beginning of an author’s name if
    the author doesn’t have an academic title
    
    also changed indentation from spaces to tabs

--- classes/Author.php
+++ classes/Author.php
@@ -10,7 +10,7 @@ class Author {
 	private $website;
 	private $contact;
 	private $text;
-    private $publications;
+	private $publications;
 
 
 	public function __construct(array $data) {
@@ -21,97 +21,102 @@ class Author {
 	}
 
 
-    /**
-     * Returns the id.
-     *
-     * @return int
-     */
-    public function getId() {
+	/**
+	 * Returns the id.
+	 *
+	 * @return int
+	 */
+	public function getId() {
 		return $this -> id;
 	}
 
 
-    /**
-     * Returns the user id or 0, if there is no user id.
-     *
-     * @return int
-     */
-    public function getUserId() {
-    	if (isset($this -> user_id)) {
-    		return $this -> user_id;
-    	}
-    	else {
-    		return 0;
-    	}
+	/**
+	 * Returns the user id or 0, if there is no user id.
+	 *
+	 * @return int
+	 */
+	public function getUserId() {
+		if (isset($this -> user_id)) {
+			return $this -> user_id;
+		}
+		else {
+			return 0;
+		}
 	}
 
 
-    /**
-     * Returns the full name, consisting of academic title, first name and last name.
-     *
-     * @return string
-     */
-    public function getName() {
-		return $this -> academic_title.' '.$this -> first_name.' '.$this -> last_name;
+	/**
+	 * Returns the full name, consisting of academic title, first name and last name.
+	 *
+	 * @return string
+	 */
+	public function getName() {
+		if (empty($this -> academic_title)) {
+			return $this -> first_name.' '.$this -> last_name;
+		}
+		else {
+			return $this -> academic_title.' '.$this -> first_name.' '.$this -> last_name;
+		}
 	}
 
 
-    /**
-     * Returns the last name.
-     *
-     * @return string
-     */
-    public function getLastName() {
+	/**
+	 * Returns the last name.
+	 *
+	 * @return string
+	 */
+	public function getLastName() {
 		return $this -> last_name;
 	}
 
 
-    /**
-     * Returns the first name.
-     *
-     * @return string
-     */
-    public function getFirstName() {
+	/**
+	 * Returns the first name.
+	 *
+	 * @return string
+	 */
+	public function getFirstName() {
 		return $this -> first_name;
 	}
 
 
-    /**
-     * Returns the academic title.
-     *
-     * @return string
-     */
-    public function getAcademicTitle() {
+	/**
+	 * Returns the academic title.
+	 *
+	 * @return string
+	 */
+	public function getAcademicTitle() {
 		return $this -> academic_title;
 	}
 
 
-    /**
-     * Returns the website.
-     *
-     * @return string
-     */
-    public function getWebsite() {
+	/**
+	 * Returns the website.
+	 *
+	 * @return string
+	 */
+	public function getWebsite() {
 		return $this -> website;
 	}
 
 
-    /**
-     * Returns the contact info.
-     *
-     * @return string
-     */
-    public function getContact() {
+	/**
+	 * Returns the contact info.
+	 *
+	 * @return string
+	 */
+	public function getContact() {
 		return $this -> contact;
 	}
 
 
-    /**
-     * Returns the author's text.
-     *
-     * @return string
-     */
-    public function getText() {
+	/**
+	 * Returns the author's text.
+	 *
+	 * @return string
+	 */
+	public function getText() {
 		return $this -> text;
 	}
 
@@ -122,20 +127,20 @@ class Author {
 	 *
 	 * @return	array
 	 */
-    public function getPublications() {
-        return $this -> publications;
-    }
+	public function getPublications() {
+		return $this -> publications;
+	}
 
 
-    /**
-     * Adds the publications to the author.
-     *
-     * @param	array	$publications	array with Publications objects
-     *
-     * @return	void
-     */
-    public function setPublications(array $publications) {
-    	$this -> publications = $publications;
-    }
+	/**
+	 * Adds the publications to the author.
+	 *
+	 * @param	array	$publications	array with Publications objects
+	 *
+	 * @return	void
+	 */
+	public function setPublications(array $publications) {
+		$this -> publications = $publications;
+	}
 
 }
