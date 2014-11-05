e1641f7e9193e9392c8e934534eb311e099a2e42
arkuuu <arkuuu@users.noreply.github.com>
2014-11-05 20:19:01 +0100

    changed to better author enumeration

--- modules/citation_styles/default.php
+++ modules/citation_styles/default.php
@@ -1,10 +1,27 @@
 <?php
 
-foreach ($publication -> getAuthors() as $author) {
- 	$citation .= $author -> getFirstName(true).' '
- 				.$author -> getLastName().', ';
+$authors = $publication -> getAuthors();
+$num = count($authors);
+
+if ($num < 1) {
+	$citation .= 'unknown author, ';
+}
+else {
+	$citation .= $authors[0] -> getFirstName(true).' '
+		 			.$authors[0] -> getLastName();
+
+	for ($i=1; $i < $num; $i++) { 
+		if ($i == $num - 1) {
+			$citation .= ' and '.$authors[$num-1] -> getFirstName(true).' '
+		 				.$authors[$num-1] -> getLastName();
+		}
+		else {
+			$citation .= ', '.$authors[$i] -> getFirstName(true).' '
+	 	 				.$authors[$i] -> getLastName();
+	 	}
+	}
 }
 
-$citation .= '"'.$publication -> getTitle().'", '
+$citation .= ', <i>"'.$publication -> getTitle().'"</i>, '
 			.$publication -> getMonth().'.'
 			.$publication -> getYear();
