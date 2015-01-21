abc36d77e99afe5be2c2863dbca8d3ed49a044f2
arkuuu <arkuuu@users.noreply.github.com>
2015-01-21 20:07:48 +0100

    added export stuff from /dev/bibtex
    this is just temporary. This should be later replaced by calling the
    BibTeX class

--- modules/export/bibtex.php
+++ modules/export/bibtex.php
@@ -3,37 +3,49 @@
 /* create list of authors */
 $authors = $publication -> getAuthors();
 $num = count($authors);
+
 $authors_string = '';
-
-if ($num < 1) {
-	$authors_string = 'Unknown Author';
+foreach ($publication -> getAuthors() as $author) {
+	$authors_string .= $author -> getName().' and ';
 }
-else {
-	$i = 1;
-	foreach ($authors as $author) {
-		if ($i == 1) {
-			$authors_string = $author -> getName();
-		}
-		else {
-			$authors_string .= ' and '.$author -> getName();
-		}
-		$i++;
-	}
+$authors_string = substr($authors_string, 0, -5);
+
+
+$key_terms_string = '';
+foreach ($publication -> getKeyTerms() as $key_term) {
+	$key_terms_string .= $key_term -> getName().', ';
 }
+$key_terms_string = substr($key_terms_string, 0, -2);
 
-/* create CiteKey */
-$citeKey = 'todo';
 
-/* TODO: temporary variables */	
-$journal = 'todo';
-$volume = 'todo';
+$data['type'] = $publication -> getTypeName();
+$data['cite_key'] = 'todo';
+$data['title'] = $publication -> getTitle();
+$data['author'] = $authors_string;
+$data['journal'] = $publication -> getJournalName();
+$data['booktitle'] = $publication -> getBookName();
+$data['publisher'] = $publication -> getPublisherName();
+// $data['edition'] = $publication -> getEdition();
+// $data['institution'] = $publication -> getInstitutionName();
+// $data['howpublished'] = $publication -> getHowpublished();
+$data['year'] = $publication -> getDatePublished('Y');
+$data['month'] = $publication -> getDatePublished('F');
+$data['volume'] = $publication -> getVolume();
+$data['pages'] = $publication -> getPages('--');
+$data['number'] = $publication -> getNumber();
+$data['series'] = $publication -> getSeries();
+$data['abstract'] = $publication -> getAbstract();
+// some more missing
+$data['keywords'] = $key_terms_string;
 
 /* create BibTeX code */
-$export = 	
-	"@".$publication -> getTypeName()."{".$citeKey.",
-		author = {".$authors_string."},
-		title = {".$publication -> getTitle()."},
-		journal = {".$journal."},
-		volume = {".$volume."},
-		year = {".$publication -> getDatePublished('Y')."},
-	}";
+$export = '@'.$data['type'].'{'.$data['cite_key'];
+
+unset($data['type'], $data['cite_key']);
+
+foreach ($data as $key => $value) {
+	if (!empty($value)) {
+		$export .= ','."\n\t".$key.' = {'.$value.'}';
+	}
+}
+$export .= "\n".'}';
