c38cf715162218c475d258e9497a8dfa4792f367
arkuuu <arkuuu@users.noreply.github.com>
2015-03-31 19:55:06 +0200

    updated database dump
    - changed collation to utf8_general_ci
    - reflected recent changes to tables

--- database.sql
+++ database.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.5.38)
 # Database: dev
-# Generation Time: 2015-03-10 11:38:46 +0000
+# Generation Time: 2015-03-31 17:50:28 +0000
 # ************************************************************
 
 
@@ -20,92 +20,79 @@
 /*!40111 SET @OLD_SQL_NOTES = @@SQL_NOTES, SQL_NOTES = 0 */;
 
 
+# Dump of table files
+# ------------------------------------------------------------
+
+CREATE TABLE `files` (
+  `id`             INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `publication_id` INT(11) UNSIGNED          DEFAULT NULL,
+  `name`           VARCHAR(100)              DEFAULT NULL,
+  `title`          VARCHAR(100)              DEFAULT NULL,
+  `full_text`      TINYINT(1)                DEFAULT '0',
+  `restricted`     TINYINT(1)                DEFAULT '0',
+  `hidden`         TINYINT(1)                DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `file_name` (`name`),
+  KEY `publication_id` (`publication_id`),
+  CONSTRAINT `files_ibfk_1` FOREIGN KEY (`publication_id`) REFERENCES `list_publications` (`id`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  COMMENT = 'Stores files and links to their publications.';
+
+
 # Dump of table list_authors
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `list_authors`;
-
 CREATE TABLE `list_authors` (
-  `id`             INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `family`         VARCHAR(50)
-                   COLLATE utf8_bin NOT NULL DEFAULT '',
-  `given`          VARCHAR(50)
-                   COLLATE utf8_bin NOT NULL DEFAULT '',
-  `academic_title` VARCHAR(20)
-                   COLLATE utf8_bin          DEFAULT NULL,
-  `website`        TEXT
-                   COLLATE utf8_bin,
-  `contact`        TEXT
-                   COLLATE utf8_bin,
-  `text`           TEXT
-                   COLLATE utf8_bin,
+  `id`       INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `family`   VARCHAR(50)      NOT NULL DEFAULT '',
+  `given`    VARCHAR(50)      NOT NULL DEFAULT '',
+  `website`  TEXT,
+  `contact`  TEXT,
+  `about`    TEXT,
+  `modified` TIMESTAMP        NULL     DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_name` (`family`, `given`)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8
-  COLLATE = utf8_bin
-  COMMENT = 'Stores all authors.';
+  COMMENT = 'Stores authors.';
 
-
-# Dump of table list_journals
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `list_journals`;
-
-CREATE TABLE `list_journals` (
-  `id`           INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name`         VARCHAR(200)
-                 COLLATE utf8_bin NOT NULL,
-  `date`         DATE                      DEFAULT NULL,
-  `publisher_id` INT(11) UNSIGNED          DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8
-  COLLATE = utf8_bin;
 
 
 # Dump of table list_keywords
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `list_keywords`;
-
 CREATE TABLE `list_keywords` (
   `id`   INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(100)
-         COLLATE utf8_bin NOT NULL DEFAULT '',
+  `name` VARCHAR(100)     NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8
-  COLLATE = utf8_bin
-  COMMENT = 'Stores all key terms.';
+  COMMENT = 'Stores keywords.';
+
 
 
 # Dump of table list_permissions
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `list_permissions`;
-
 CREATE TABLE `list_permissions` (
   `id`   INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(50)
-         COLLATE utf8_bin NOT NULL DEFAULT '',
+  `name` VARCHAR(50)      NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8
-  COLLATE = utf8_bin;
+  COMMENT = 'Stores permissions.';
+
 
 
 # Dump of table list_publications
 # ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `list_publications`;
 
 CREATE TABLE `list_publications` (
   `id`             INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -113,44 +100,28 @@ CREATE TABLE `list_publications` (
   COMMENT 'Links to the type name.',
   `study_field_id` INT(11) UNSIGNED          DEFAULT NULL
   COMMENT 'Links to the study field name.',
-  `title`          VARCHAR(200)
-                   COLLATE utf8_bin NOT NULL DEFAULT '',
+  `title`          VARCHAR(200)     NOT NULL DEFAULT '',
   `date_added`     TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `date_published` DATE                      DEFAULT NULL,
-  `booktitle`      VARCHAR(200)
-                   COLLATE utf8_bin          DEFAULT NULL,
-  `journal`        VARCHAR(200)
-                   COLLATE utf8_bin          DEFAULT NULL,
+  `booktitle`      VARCHAR(200)              DEFAULT NULL,
+  `journal`        VARCHAR(200)              DEFAULT NULL,
   `volume`         INT(5) UNSIGNED           DEFAULT NULL,
   `number`         INT(5) UNSIGNED           DEFAULT NULL,
   `pages_from`     INT(5) UNSIGNED           DEFAULT NULL,
   `pages_to`       INT(5) UNSIGNED           DEFAULT NULL,
-  `series`         VARCHAR(50)
-                   COLLATE utf8_bin          DEFAULT NULL,
-  `edition`        VARCHAR(50)
-                   COLLATE utf8_bin          DEFAULT NULL,
-  `note`           VARCHAR(200)
-                   COLLATE utf8_bin          DEFAULT NULL,
-  `location`       VARCHAR(50)
-                   COLLATE utf8_bin          DEFAULT NULL,
-  `publisher`      VARCHAR(200)
-                   COLLATE utf8_bin          DEFAULT NULL,
-  `institution`    VARCHAR(200)
-                   COLLATE utf8_bin          DEFAULT NULL,
-  `school`         VARCHAR(200)
-                   COLLATE utf8_bin          DEFAULT NULL,
-  `address`        VARCHAR(200)
-                   COLLATE utf8_bin          DEFAULT NULL,
-  `isbn`           VARCHAR(20)
-                   COLLATE utf8_bin          DEFAULT NULL,
-  `doi`            VARCHAR(50)
-                   COLLATE utf8_bin          DEFAULT NULL,
-  `howpublished`   VARCHAR(200)
-                   COLLATE utf8_bin          DEFAULT NULL,
-  `abstract`       TEXT
-                   COLLATE utf8_bin,
-  `copyright`      VARCHAR(200)
-                   COLLATE utf8_bin          DEFAULT NULL,
+  `series`         VARCHAR(50)               DEFAULT NULL,
+  `edition`        VARCHAR(50)               DEFAULT NULL,
+  `note`           VARCHAR(200)              DEFAULT NULL,
+  `location`       VARCHAR(50)               DEFAULT NULL,
+  `publisher`      VARCHAR(200)              DEFAULT NULL,
+  `institution`    VARCHAR(200)              DEFAULT NULL,
+  `school`         VARCHAR(200)              DEFAULT NULL,
+  `address`        VARCHAR(200)              DEFAULT NULL,
+  `isbn`           VARCHAR(20)               DEFAULT NULL,
+  `doi`            VARCHAR(50)               DEFAULT NULL,
+  `howpublished`   VARCHAR(200)              DEFAULT NULL,
+  `abstract`       TEXT,
+  `copyright`      VARCHAR(200)              DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `title` (`title`),
   KEY `publication_to_type_idx` (`type_id`),
@@ -164,130 +135,101 @@ CREATE TABLE `list_publications` (
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8
-  COLLATE = utf8_bin
-  COMMENT = 'Stores all publications. Authors and key terms can be found by joining the relation tables.';
+  COMMENT = 'Stores all publications.';
 
-
-# Dump of table list_publishers
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `list_publishers`;
-
-CREATE TABLE `list_publishers` (
-  `id`   INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(200)
-         COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8
-  COLLATE = utf8_bin;
-
-
-# Dump of table list_references
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `list_references`;
-
-CREATE TABLE `list_references` (
-  `id`                 INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `ref_publication_id` INT(11) UNSIGNED          DEFAULT NULL
-  COMMENT 'Links to an existing publication.',
-  `text`               TEXT
-                       COLLATE utf8_bin,
-  `external_url`       TEXT
-                       COLLATE utf8_bin,
-  PRIMARY KEY (`id`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8
-  COLLATE = utf8_bin
-  COMMENT = 'Stores all references, either linking to an existing publication or an external one. This table is still TODO';
 
 
 # Dump of table list_roles
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `list_roles`;
-
 CREATE TABLE `list_roles` (
   `id`   INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(20)
-         COLLATE utf8_bin NOT NULL DEFAULT '',
+  `name` VARCHAR(20)      NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8
-  COLLATE = utf8_bin;
+  COMMENT = 'Stores roles.';
+
 
 
 # Dump of table list_study_fields
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `list_study_fields`;
-
 CREATE TABLE `list_study_fields` (
-  `id`   INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(100)
-         COLLATE utf8_bin NOT NULL DEFAULT '',
+  `id`          INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name`        VARCHAR(100)     NOT NULL DEFAULT '',
+  `description` TEXT,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8
-  COLLATE = utf8_bin
-  COMMENT = 'Stores all study fields.';
+  COMMENT = 'Stores fields of study.';
+
 
 
 # Dump of table list_types
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `list_types`;
-
 CREATE TABLE `list_types` (
-  `id`   INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(20)
-         COLLATE utf8_bin NOT NULL,
+  `id`          INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name`        VARCHAR(20)      NOT NULL,
+  `description` TEXT,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8
-  COLLATE = utf8_bin
-  COMMENT = 'Stores all types of publications. Uses known BibTeX types.';
+  COMMENT = 'Stores types of publications.';
+
 
 
 # Dump of table list_users
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `list_users`;
-
 CREATE TABLE `list_users` (
   `id`              INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name`            VARCHAR(40)
-                    COLLATE utf8_bin NOT NULL,
+  `name`            VARCHAR(40)      NOT NULL,
   `password`        VARCHAR(50)
-                    COLLATE utf8_bin NOT NULL,
-  `mail`            VARCHAR(40)
-                    COLLATE utf8_bin NOT NULL,
+                    CHARACTER SET utf8
+                    COLLATE utf8_bin NOT NULL
+  COMMENT 'stores passwords. Therefore uses case sensitive collation',
+  `mail`            VARCHAR(40)      NOT NULL,
   `date_register`   TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `date_last_login` TIMESTAMP        NULL     DEFAULT NULL,
   `active`          TINYINT(1)       NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
+  UNIQUE KEY `name` (`name`),
+  UNIQUE KEY `mail` (`mail`)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8
-  COLLATE = utf8_bin
-  COMMENT = 'Stores all registered users.';
+  COMMENT = 'Stores registered users.';
+
+
+# Dump of table oai_tokens
+# ------------------------------------------------------------
+
+CREATE TABLE `oai_tokens` (
+  `id`              INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `created`         TIMESTAMP        NULL     DEFAULT CURRENT_TIMESTAMP,
+  `metadata_prefix` VARCHAR(10)               DEFAULT NULL,
+  `from`            VARCHAR(50)               DEFAULT NULL,
+  `until`           VARCHAR(50)               DEFAULT NULL,
+  `set`             VARCHAR(100)              DEFAULT NULL,
+  `cursor`          INT(11) UNSIGNED          DEFAULT NULL,
+  `list_size`       INT(11) UNSIGNED          DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  COMMENT = 'Stores the tokens of the OAI interface.';
 
 
 # Dump of table rel_publ_to_authors
 # ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `rel_publ_to_authors`;
 
 CREATE TABLE `rel_publ_to_authors` (
   `id`             INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -305,40 +247,12 @@ CREATE TABLE `rel_publ_to_authors` (
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8
-  COLLATE = utf8_bin
-  COMMENT = 'Links a publication to it’s authors.';
-
-
-# Dump of table rel_publ_to_references
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `rel_publ_to_references`;
-
-CREATE TABLE `rel_publ_to_references` (
-  `id`             INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `publication_id` INT(11) UNSIGNED NOT NULL,
-  `reference_id`   INT(11) UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `rel_references_to_reference_idx` (`reference_id`),
-  KEY `rel_references_to_publ_idx` (`publication_id`),
-  CONSTRAINT `rel_references_to_publ` FOREIGN KEY (`publication_id`) REFERENCES `list_publications` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `rel_references_to_reference` FOREIGN KEY (`reference_id`) REFERENCES `list_references` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8
-  COLLATE = utf8_bin
-  COMMENT = 'Links a publication to it’s references.';
+  COMMENT = 'Links publications to their authors.';
 
 
 
 # Dump of table rel_publication_keywords
 # ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `rel_publication_keywords`;
 
 CREATE TABLE `rel_publication_keywords` (
   `id`             INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -356,19 +270,17 @@ CREATE TABLE `rel_publication_keywords` (
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8
-  COLLATE = utf8_bin
-  COMMENT = 'Links a publication to it’s key terms.';
+  COMMENT = 'Links publications to their keywords';
+
 
 
 # Dump of table rel_roles_permissions
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `rel_roles_permissions`;
-
 CREATE TABLE `rel_roles_permissions` (
   `id`            INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `role_id`       INT(10) UNSIGNED NOT NULL,
-  `permission_id` INT(10) UNSIGNED NOT NULL,
+  `role_id`       INT(11) UNSIGNED NOT NULL,
+  `permission_id` INT(11) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_entries` (`role_id`, `permission_id`),
   KEY `to_permissions` (`permission_id`),
@@ -377,18 +289,16 @@ CREATE TABLE `rel_roles_permissions` (
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8
-  COLLATE = utf8_bin;
+  COMMENT = 'Links roles to their permissions.';
 
 
 # Dump of table rel_user_roles
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `rel_user_roles`;
-
 CREATE TABLE `rel_user_roles` (
   `id`      INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `user_id` INT(10) UNSIGNED NOT NULL,
-  `role_id` INT(10) UNSIGNED NOT NULL,
+  `user_id` INT(11) UNSIGNED NOT NULL,
+  `role_id` INT(11) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_entry` (`user_id`, `role_id`),
   KEY `to_role` (`role_id`),
@@ -397,7 +307,7 @@ CREATE TABLE `rel_user_roles` (
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8
-  COLLATE = utf8_bin;
+  COMMENT = 'Links users to their roles.';
 
 
 /*!40111 SET SQL_NOTES = @OLD_SQL_NOTES */;
