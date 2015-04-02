7a10c17cb801e71d106342bad9d5441b0ca7ae06
arkuuu <arkuuu@users.noreply.github.com>
2015-04-02 15:59:56 +0200

    implemented search for publications
    search for publications' authors and keywords is not available yet

--- src/SearchController.php
+++ src/SearchController.php
@@ -3,23 +3,99 @@
 
 namespace publin\src;
 
+use UnexpectedValueException;
+
 class SearchController {
 
 	private $db;
 	private $model;
+	private $result;
+	private $errors;
 
 
 	public function __construct(Database $db) {
 
-		$this->db = $db;
+		$this->db = new PDODatabase();
 		$this->model = new SearchModel($db);
+		$this->result = array();
+		$this->errors = array();
 	}
 
 
 	public function run(Request $request) {
 
-		$view = new SearchView();
+		if ($request->get('type') === 'publication') {
+			$this->searchPublications($request);
+		}
+
+		$view = new SearchView($this->result, $this->errors);
 
 		return $view->display();
+	}
+
+
+	public function searchPublications(Request $request) {
+
+		$field = Validator::sanitizeText($request->get('field'));
+		$search = Validator::sanitizeText($request->get('search'));
+		if (!$search) {
+			$this->errors[] = 'Your search input is invalid';
+
+			return false;
+		}
+
+		$search_words = explode(' ', $search);
+
+		$repo = new PublicationRepository($this->db);
+		$repo->select();
+
+		switch (true) {
+			case $field === 'title':
+				foreach ($search_words as $word) {
+					$repo->where('title', 'LIKE', '%'.$word.'%');
+				}
+				break;
+			case $field === 'booktitle':
+				foreach ($search_words as $word) {
+					$repo->where('booktitle', 'LIKE', '%'.$word.'%');
+				}
+				break;
+			case $field === 'journal':
+				foreach ($search_words as $word) {
+					$repo->where('journal', 'LIKE', '%'.$word.'%');
+				}
+				break;
+			case $field === 'publisher':
+				foreach ($search_words as $word) {
+					$repo->where('publisher', 'LIKE', '%'.$word.'%');
+				}
+				break;
+			case $field === 'year':
+				$repo->where('date_published', 'LIKE', $search, 'YEAR');
+				break;
+			case $field === 'abstract':
+				foreach ($search_words as $word) {
+					$repo->where('abstract', 'LIKE', '%'.$word.'%');
+				}
+				break;
+			default:
+				throw new UnexpectedValueException;
+		}
+
+		$this->result = $repo->order('date_published', 'DESC')->find();
+
+		return true;
+	}
+
+
+	public function searchAll(Request $request) {
+
+		return false;
+	}
+
+
+	public function searchAuthors(Request $request) {
+
+		return false;
 	}
 }
--- src/SearchView.php
+++ src/SearchView.php
@@ -5,14 +5,40 @@ namespace publin\src;
 
 class SearchView extends View {
 
-	public function __construct() {
+	private $results;
 
-		parent::__construct('search');
+
+	public function __construct(array $results, array $errors = array()) {
+
+		parent::__construct('search', $errors);
+		$this->results = $results;
 	}
 
 
 	public function showTitle() {
 
 		return 'Search';
+	}
+
+
+	public function showResults() {
+
+		$string = '';
+		$author_url = './?p=author&id=';
+		if (!empty($this->results)) {
+			foreach ($this->results as $result) {
+				if ($result instanceof Publication) {
+					$string .= '<li>'.$this->showCitation($result).'</li>';
+				}
+				else if ($result instanceof Author) {
+					$string .= '<li><a href="'.$this->html($author_url.$result->getId()).'">'.$this->html($result->getName()).'</a></li>';
+				}
+			}
+
+			return '<ul>'.$string.'</ul>';
+		}
+		else {
+			return false;
+		}
 	}
 }
